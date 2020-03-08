a92ff56e6deb36a07956d138d97126ec58e0e7ec
sdf <trohimovkavad@gmail.com>
2020-03-08 14:05:33 +0200

    add help rich message

--- Messages/RichEmbed/Help.go
+++ Messages/RichEmbed/Help.go
@@ -1,1 +1,15 @@
 package RichEmbed
+
+import (
+	"github.com/bwmarrin/discordgo"
+)
+
+var Help discordgo.MessageEmbed = discordgo.MessageEmbed{
+	Title: "Commands for color selection:",
+	Color: 16711680,
+	Fields: []*discordgo.MessageEmbedField{{
+		Name:   "Color-hex gives information about colors",
+		Value:  "```!color #d81e4c``` ```!color #00ff15```",
+		Inline: true,
+	}},
+}
