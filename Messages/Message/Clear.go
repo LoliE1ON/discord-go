1d9c7e786166c74136b4873232aee95f987ae8a2
sdf <trohimovkavad@gmail.com>
2020-03-08 18:26:20 +0200

    clear role color

--- Messages/Message/Clear.go
+++ Messages/Message/Clear.go
@@ -1,3 +1,31 @@
 package Message
 
-func 
+import (
+	"log"
+
+	"github.com/LoliE1ON/discord-go/Messages/RichEmbed"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// Clear role color
+func Clear(s *discordgo.Session, m *discordgo.MessageCreate) {
+
+	if m.Content == "!clear" {
+
+		// Remove roles
+		err := RemoveUserRoles(s, m)
+		if err != nil {
+			log.Println("Clear role", err)
+			return
+		}
+
+		// Reply
+		_, err = s.ChannelMessageSendEmbed(m.ChannelID, &RichEmbed.Clear)
+		if err != nil {
+			log.Println("Clear role", err)
+			return
+		}
+	}
+
+}
