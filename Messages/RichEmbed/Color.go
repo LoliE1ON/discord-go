961ca0af66b380f51a502e29a0f6a9ed3e6f4d90
sdf <trohimovkavad@gmail.com>
2020-03-08 13:53:35 +0200

    add rich messages for color request

--- Messages/RichEmbed/Color.go
+++ Messages/RichEmbed/Color.go
@@ -1,13 +1,30 @@
-package Messages
+package RichEmbed
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"github.com/LoliE1ON/discord-go/Helpers/HexHelper"
+	"github.com/bwmarrin/discordgo"
+)
 
-var ColorIncorrect = discordgo.MessageEmbed{
-	Title:       "",
-	Color:       0,
-	Fields:      nil,
+var ColorIncorrect discordgo.MessageEmbed = discordgo.MessageEmbed{
+	Title: "Color incorrect",
+	Fields: []*discordgo.MessageEmbedField{{
+		Name:   "Color-HEX gives information about colors",
+		Value:  "You entered the wrong color. Need to enter Color-HEX",
+		Inline: true,
+	}},
 }
 
-.setColor('#ff0000')
-.setTitle('Color incorrect')
-.addField('Color-HEX gives information about colors', 'You entered the wrong color. Need to enter Color-HEX', true)
+func ColorAssign(color string) (message discordgo.MessageEmbed, err error) {
+
+	hexInt, err := HexHelper.HexToInt(color)
+	if err != nil {
+		return
+	}
+
+	message = discordgo.MessageEmbed{
+		Title: "Color successfully assigned",
+		Color: hexInt,
+	}
+
+	return
+}
